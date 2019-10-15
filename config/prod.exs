ce579540962d8e5ba7430ab068560426e6edeafd
John Mertens <john.mertens@gmail.com>
2019-10-14 22:46:51 -0700

    Can't put <> in config file

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :clicky, ClickyWeb.Endpoint,
-  http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  url: [host: ~s[#{System.get_env("APP_NAME")}.gigalixirapp.com], port: 80],
   server: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
